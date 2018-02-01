c2b11e1a1e3eb4d3f282bb845eda0ec432dc4af6
dave <dave@bould.net>
2018-02-01 20:38:21 +0000

    Test passed in number gets returned if not 5 or 3

--- FizzBuzzKata/Kata.py
+++ FizzBuzzKata/Kata.py
@@ -2,3 +2,5 @@ class Kata:
     def run(self, test_case):
         if test_case % 3 == 0:
             return "Fizz"
+        else:
+            return test_case
--- test/test_kata.py
+++ test/test_kata.py
@@ -2,6 +2,11 @@ from FizzBuzzKata import Kata
 
 
 class TestKata:
-    def test_data_gets_returned(self):
+
+    def test_three_prints_fizz(self):
         my_kata = Kata()
         assert my_kata.run(3) == "Fizz"
+
+    def test_number_gets_returned(self):
+        my_kata = Kata()
+        assert my_kata.run(4) == 4
